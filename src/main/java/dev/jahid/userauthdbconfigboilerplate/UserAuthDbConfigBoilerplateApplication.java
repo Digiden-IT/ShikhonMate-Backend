9922fab05b582a075b7db5d748353b911120fe69
jahid <jahidctgcse@gmail.com>
2025-01-14 19:08:36 +0600

    added initial setup

---
+++ src/main/java/dev/jahid/userauthdbconfigboilerplate/UserAuthDbConfigBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package dev.jahid.userauthdbconfigboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthDbConfigBoilerplateApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( UserAuthDbConfigBoilerplateApplication.class, args );
+    }
+
+}
---
+++ src/test/java/dev/jahid/userauthdbconfigboilerplate/UserAuthDbConfigBoilarplateApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jahid.userauthdbconfigboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthDbConfigBoilarplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

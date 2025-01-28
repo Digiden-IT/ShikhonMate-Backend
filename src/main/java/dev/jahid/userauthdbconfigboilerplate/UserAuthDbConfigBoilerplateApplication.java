aeb41d6d88cc6777a5fd94bdbcca765eae3aa92e
jahid <jahidctgcse@gmail.com>
2025-01-28 12:49:44 +0600

    Modified gitignore to ignore the gitignore and application-test.yml

--- src/main/java/dev/jahid/userauthdbconfigboilerplate/UserAuthDbConfigBoilerplateApplication.java
+++ src/main/java/dev/jahid/userauthdbconfigboilerplate/UserAuthDbConfigBoilerplateApplication.java
@@ -9,5 +9,4 @@ public class UserAuthDbConfigBoilerplateApplication {
     public static void main( String[] args ) {
         SpringApplication.run( UserAuthDbConfigBoilerplateApplication.class, args );
     }
-
 }

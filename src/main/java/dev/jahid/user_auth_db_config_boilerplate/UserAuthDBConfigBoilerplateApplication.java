302bedcaddc050b3f8cac4e817181835e456c3dc
jahid <jahidctgcse@gmail.com>
2025-02-14 23:18:32 +0600

    fix: space issue in UserAuthDBConfigBoilerplateApplication

--- src/main/java/dev/jahid/user_auth_db_config_boilerplate/UserAuthDBConfigBoilerplateApplication.java
+++ src/main/java/dev/jahid/user_auth_db_config_boilerplate/UserAuthDBConfigBoilerplateApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserAuthDBConfigBoilerplateApplication {
+
     public static void main( String[] args ) {
         SpringApplication.run( UserAuthDBConfigBoilerplateApplication.class, args );
     }
